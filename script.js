ce1bfbf3eb19fd55919f5218c93a013ffe6da209
Christopher Phillips <cphillips918@gmail.com>
2016-07-08 22:17:01 -0400

    created object with brain

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
+var simon = {
+    "greenButton": {
+        "id": 1,
+        response: function(){
+            
+        }
+    },
+    "redbutton": {
+        "id": 2,
+        response: function(){
+            console.log('iworkred')
+        }
+    },
+    "yellowButton": {
+        "id": 3,
+        response: function(){
+            console.log('iworkyellow')
+        }
+    },
+    "blueButton": {
+        "id": 4,
+        response: function(){
+            console.log('iworkblue')
+        }
+    },
+    "brain": {
+        "userMoveSet":[],
+        "simonMoveSet":[],
+        simonSays: function(){
+            var theMove = Math.floor(Math.random() * (5 - 1)) + 1;
+            simon.brain.simonMoveSet.push(theMove)
+            for(i=0;i<this.simonMoveSet.length;i++){
+                switch(this.simonMoveSet[i]){
+                    case 1:
+                        simon.greenButton.response();
+                        break;
+                    case 2:
+                        simon.redbutton.response();
+                        break;
+                    case 3:
+                        simon.yellowButton.response();
+                        break;
+                    case 4:
+                        simon.blueButton.response();
+                        break;
+                }
+
+            }
+
+        },
+
+    }
+}
+
+
+
+
+
 $(document).ready(function() {
     console.log('iwork')
 
 });
-
-
-//simon object
-    //greenButton
-        //method//changecolor/playsound
-        //property//UniqueID:0
-    //redButton
-        //method//changecolor/playsound
-        //property//UniqueID:1
-    //yellowButton
-        //method//changecolor/playsound
-        //property//UniqueID:2
-    //blueButton
-        //method//changecolor/playsound
-        //property//UniqueID:3
-    //brain
-        //property//userMoveArray
-        //property//simonMoveArray
-        //method//simonSays()
-                //call rng (0,1,2,3),
-                //push rng to simonarray
-                //have loop itterate over simon array and call the function associate with array value (can set interval so that it executes at certain rate)
-    //roundCounter is equal to simonarray.length
-
-//docReady
-    //onClick(gryb)
-        //callUniqeButtonObjectFuntion
-        //push UniqueID to PlayerArray
-        //if player array = simonarray(maybe sum the numbers?)
-            //callSimonSays
-        //else end the game
-    //onClickStart
-        //callSimonSays
